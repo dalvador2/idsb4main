6ca49ce63b26aa47566542dc1094cf0b28b48d7f
dalvador2 <torin@storkey.co>
2023-01-16 13:03:51 +0000

    cleanup

--- db_classes/errors.py
+++ db_classes/errors.py
@@ -1,5 +1,6 @@
 class PresenceError(Exception):
     pass
+
 class RecordError(Exception):
     def __init__(self) -> None:
         self.message = "parameters were underspecified for the target function"
